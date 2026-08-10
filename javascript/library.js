cfc227f0d6492a1b7bfec7b8a1297448b525780f
aleksi- <allulii09@gmail.com>
2026-08-10 14:03:42 +0300

    odin javascript kirjasto

--- javascript/library.js
+++ javascript/library.js
@@ -1,8 +1,4 @@
-/* 
-  1. html, miten saadaan inputtien tiedot funktio kutsuun ja listättyä array taulukon jatkoksi. 
-  2. miten saat listan näkyville html:ssä, kun tiedot on syötetty. 
-  
-*/
+
 
 const myLibrary = [{
     title: "Tuntematon sotilas",
@@ -15,7 +11,7 @@ const myLibrary = [{
         this.title = title;
         this.author = author; 
         this.pages =pages;
-}
+    }
 
 function addBookToLibrary(title, author, pages) {
     
@@ -27,17 +23,27 @@ function addBookToLibrary(title, author, pages) {
 
     myLibrary.push(newBook);
     
+    displayLibrary()
+    }
     
-}
+    function displayLibrary() {
+      const libraryContainer = document.getElementById("libraryContainer");
+      libraryContainer.innerHTML = "";
+    
+      myLibrary.forEach(book => {
+        const card = document.createElement("div");
+        card.classList.add("book-card");
+        card.innerHTML = `
+          <p>${book.title}</p>
+          <p>${book.author}</p>
+          <p>${book.pages}</p>
+        `;
+        libraryContainer.appendChild(card);
+      });  
+    
+    }
+      
 
-function displayLibrary() {
-    const libraryContainer = document.getElementById("library");
-    libraryContainer.innerHTML = "";
-}
 
-myLibrary.forEach ((book, index) => {
-  const card = document.createElement("div");
-  card.classList.add("book-card");
-});
 
 
