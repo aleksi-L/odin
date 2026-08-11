7d1903b5cc273484c2a3e97bd3df531b664e2f46
aleksi- <allulii09@gmail.com>
2026-08-11 14:18:31 +0300

    library toivottavasti vika commit en kun on valmista

--- javascript/library.js
+++ javascript/library.js
@@ -1,49 +1,61 @@
-
-
-const myLibrary = [{
+const myLibrary = [
+  {
     title: "Tuntematon sotilas",
     author: "Väinö Linna",
-    pages: "500"
+    pages: "500",
+    isRead: false
   }
 ];
 
-    function Book(title, author, pages) {
-        this.title = title;
-        this.author = author; 
-        this.pages =pages;
-    }
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = false;
+}
 
-function addBookToLibrary(title, author, pages) {
-    
-    let a = document.getElementById("title").value;
-    let b = document.getElementById("author").value;
-    let c = document.getElementById("pages").value;
+Book.prototype.toggleRead = function() {
+  this.isRead = !this.isRead;
+};
 
-    const newBook = new Book(a, b, c,);
+function addBookToLibrary() {
+  let a = document.getElementById("title").value;
+  let b = document.getElementById("author").value;
+  let c = document.getElementById("pages").value;
 
-    myLibrary.push(newBook);
-    
-    displayLibrary()
-    }
-    
-    function displayLibrary() {
-      const libraryContainer = document.getElementById("libraryContainer");
-      libraryContainer.innerHTML = "";
-    
-      myLibrary.forEach(book => {
-        const card = document.createElement("div");
-        card.classList.add("book-card");
-        card.innerHTML = `
-          <p>${book.title}</p>
-          <p>${book.author}</p>
-          <p>${book.pages}</p>
-        `;
-        libraryContainer.appendChild(card);
-      });  
-    
-    }
-      
+  const newBook = new Book(a, b, c);
+  myLibrary.push(newBook);
 
+  displayLibrary();
+}
 
+function displayLibrary() {
+  const libraryContainer = document.getElementById("libraryContainer");
+  libraryContainer.innerHTML = "";
 
+  myLibrary.forEach((book, index) => {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
 
+    card.innerHTML = `
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <p>${book.pages}</p>
+      <p>Status: ${book.isRead ? "Read" : "Not read"}</p>
+      <button onclick="removeBook(${index})">Remove</button>
+      <button onclick="toggleReadStatus(${index})">Toggle Read Status</button>
+    `;
+
+    libraryContainer.appendChild(card);
+  });
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayLibrary();
+}
+
+function toggleReadStatus(index) {
+  myLibrary[index].toggleRead();
+  displayLibrary();
+}
