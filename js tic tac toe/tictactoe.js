f5c9ae0d4dc51c58a0c70779a643c4349d7c7089
aleksi- <allulii09@gmail.com>
2026-08-31 14:15:50 +0300

    tictactoe

--- js tic tac toe/tictactoe.js
+++ js tic tac toe/tictactoe.js
@@ -1,48 +1,55 @@
-(function () {
-  const board = ["", "", "", "", "", "", "", "", ""];
-  let player = "X";
-  let over = false;
+const Gameboard = (() => {
+    const board = Array(9).fill("");
 
-  const wins = [
-    [0,1,2],[3,4,5],[6,7,8],
-    [0,3,6],[1,4,7],[2,5,8],
-    [0,4,8],[2,4,6]
-  ];
+    const getBoard = () => board;
+    
+    let turnO = true;
 
-  const boardEl = document.getElementById("board");
-  const msgEl = document.getElementById("msg");
-
-  function draw() {
-    boardEl.innerHTML = "";
-    board.forEach((v,i) => {
-      const c = document.createElement("button");
-      c.style.width = "60px";
-      c.style.height = "60px";
-      c.style.fontSize = "30px";
-      c.textContent = v;
-      c.onclick = () => move(i);
-      boardEl.appendChild(c);
-    });
-  }
-
-  function move(i) {
-    if (board[i] || over) return;
-    board[i] = player;
-
-    if (wins.some(w => w.every(x => board[x] === player))) {
-      msgEl.textContent = player + " wins!";
-      over = true;
-    } else if (board.every(x => x)) {
-      msgEl.textContent = "Draw!";
-      over = true;
-    } else {
-      player = player === "X" ? "O" : "X";
-      msgEl.textContent = player + "'s turn";
+    const placeMarker = (index, marker) => {
+      if (board[index] === "") {
+        board[index] = marker;
+        return true;
     }
+      return false;
+    };
 
-    draw();
+    const reset = () => board.fill(""); 
+
+    return { getBoard, placeMarker, reset };
+})()
+
+const voittolinijat = [
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [3,4,5],
+  [6,7,8],
+  [2,4,6],
+]
+
+ruudut.forEach(ruutu => {
+  ruutu.addEventListener('click', function () {
+  if (turn0) {
+  ruutu.innerText = 'O';
+  turn0 = false;
   }
+ else {
+  ruutu.innerText = 'X';
+  turn0 = true;
+  }
+  });
+});
+const enableruudut = () => {
+  for (let ruutu of ruudut) {
+      ruutu.disabled = false;
+      ruutu.innerText = "";
+  }
+};
 
-  msgEl.textContent = player + "'s turn";
-  draw();
-})();
+const disableruudut = () => {
+  for (let ruutu of ruudut) {
+      ruutu.disabled = true;
+  }
+};
